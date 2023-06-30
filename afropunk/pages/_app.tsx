84efea86cf31d39fce7b136609ab635fcf8a0adb
LucasTBorges <lucastborges@gmail.com>
2023-06-30 20:31:52 -0300

    Remoção de bloco de código comentado do _app.tsx

--- afropunk/pages/_app.tsx
+++ afropunk/pages/_app.tsx
@@ -1,22 +1,4 @@
 import '@/styles/globals.css'
-/* import type { AppProps } from 'next/app'
-import { SessionProvider } from "next-auth/react"
-import { NextComponentType, NextPageContext } from 'next';
-
-interface IProps{
-  session: any;
-  Component: NextComponentType<NextPageContext, any, any>;
-  pageProps: any;
-}
-
-export default function App({ session, Component, pageProps }: IProps) {
-  return ( 
-    //<SessionProvider session={session}>
-      <Component {...pageProps} />
-    //</SessionProvider>
-  )
-} */
-
 import { SessionProvider } from "next-auth/react"
 import type { NextComponentType, NextPageContext } from 'next';
 

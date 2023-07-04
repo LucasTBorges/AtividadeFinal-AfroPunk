d30ace6f87d9d73b3736b6c5e2e2d2717e39b818
BeHaga <brunohenriqueluz5@gmail.com>
2023-07-04 00:14:38 -0300

    implementacao da galeria na tela principal

---
+++ afropunk/pages/components/galeria.tsx
@@ -0,0 +1,34 @@
+import styles from '@/styles/galeria.module.css'
+import Image from 'next/image'
+import Foto1 from './../../public/images/Rectangle 132.svg'
+import Foto2 from './../../public/images/Rectangle 133.svg'
+import Foto3 from './../../public/images/Rectangle 134.svg'
+import Foto4 from './../../public/images/Rectangle 135.svg'
+import Foto5 from './../../public/images/Rectangle 136.svg'
+import Foto6 from './../../public/images/Rectangle 137.svg'
+import Foto7 from './../../public/images/Rectangle 138.svg'
+import Foto8 from './../../public/images/Rectangle 139.svg'
+import Foto9 from './../../public/images/Rectangle 140.svg'
+
+export default function Galeria() {
+  return (
+    <>
+        <section className={styles.section}>
+            <div className={styles.div1}>
+                <h2>Galeria</h2>
+            </div>
+            <div className={styles.div2}>
+                <Image src={Foto1} alt="" />
+                <Image src={Foto2} alt="" />
+                <Image src={Foto3} alt="" />
+                <Image src={Foto4} alt="" />
+                <Image src={Foto5} alt="" />
+                <Image src={Foto6} alt="" />
+                <Image src={Foto7} alt="" />
+                <Image src={Foto8} alt="" />
+                <Image src={Foto9} alt="" />
+            </div>
+        </section>
+    </>
+  )
+}
--- afropunk/pages/index.tsx
+++ afropunk/pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
 import Header from './components/header'
+import Galeria from './components/galeria'
 import Footer from './components/footer'
 
 export default function Home() {
@@ -15,6 +16,7 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         <Header />
+        <Galeria />
         <Footer />
       </main>
     </>

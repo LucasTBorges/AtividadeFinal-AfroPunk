396cf95e5f88d60593b846405bc9567364b5e7e8
BeHaga <brunohenriqueluz5@gmail.com>
2023-07-05 12:15:07 -0300

    finalizacao da area de ingressos

--- afropunk/pages/components/ingressos.tsx
+++ afropunk/pages/components/ingressos.tsx
@@ -6,14 +6,23 @@ export default function Ingressos() {
   return (
     <>
         <section className={styles.section}>
-            <h2>Ingressos</h2>
-            <h3>Escolha entre as opções de ingressos disponíveis e viva o AFROPUNK, uma experiência única.</h3>
+            <div className={styles.text}>
+              <div className={styles.divh2}>
+                <div className={styles.linha1}></div>
+                <h2>Ingressos</h2>
+                <div className={styles.linha2}></div>
+              </div>
+              <h3 id={styles.h3top}>Escolha entre as opções de ingressos disponíveis</h3>
+              <h3>e viva o AFROPUNK, uma experiência única.</h3>
+            </div>            
             <div className={styles.ingressos}>
                 <Ingresso tipo="Meia" valor="R$85,00" />
                 <Ingresso tipo="Meia social" valor="R$95,00" />
                 <Ingresso tipo="Inteira" valor="R$130,00" />
             </div>
-            <a>COMPRAR INGRESSO</a>
+            <div className={styles.bottom}>
+              <a>COMPRAR INGRESSO</a>
+            </div>
         </section>
     </>
   )

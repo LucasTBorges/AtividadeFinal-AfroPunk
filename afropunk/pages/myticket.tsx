c6322f61ad17d5d3a170d50bc21b443652d7ad00
brunaanunciacao <bruna.anunciacao@ufba.br>
2023-07-13 15:10:04 -0300

    puxando os ingressos do usuario e exibindo na tela

--- afropunk/pages/myticket.tsx
+++ afropunk/pages/myticket.tsx
@@ -1,21 +1,70 @@
-import LoginHeader from "./components/loginheader"
-import styles from "@/styles/myticket.module.css"
+import LoginHeader from "./components/loginheader";
+import styles from "@/styles/myticket.module.css";
+import { useState, useEffect } from "react";
+import { IngressoObj } from "@/services/services";
 export default function MyTicket() {
-    
-    return (
-        <>
-        <main className={styles.main}>
-            <LoginHeader/>
-            <div>
-                <div>
-                    <h1>Meus ingressos</h1>
-                </div>
-                <div>
-                    <p>Ativos</p>
-                    <p>Encerrados</p>
-                </div>
-            </div>
-        </main>
-        </>
-    )
+  const [active, setActive] = useState<boolean>(false);
+  const [closed, setClosed] = useState<boolean>(false);
+  const [tickets, setTickets] = useState<IngressoObj[] | null>(null);
+  const handleActive = () => {
+    setActive(true);
+    setClosed(false);
+  };
+
+  const handleClosed = () => {
+    setClosed(true);
+    setActive(false);
+  };
+  useEffect(() => {
+    const fetchTickets = async () => {
+      try {
+        const response = await fetch("/api/usuario/ingressos");
+        if (response.ok) {
+          const data = await response.json();
+          setTickets(data);
+        } else {
+          console.log("Erro ao obter os ingressos:", response.status);
+        }
+      } catch (error) {
+        console.log("Erro ao obter os ingressos:", error);
+      }
+    };
+    fetchTickets();
+  }, []);
+
+  return (
+    <>
+      <main className={styles.main}>
+        <LoginHeader />
+        <section>
+          <div className={styles.title}>
+            <h1>Meus ingressos</h1>
+          </div>
+          <div className={styles.menu}>
+            <button onClick={handleActive}>Ativos</button>
+            <button onClick={handleClosed}>Encerrados</button>
+          </div>
+          <div className={styles.myticket}>
+            {active && (
+              <>
+                {tickets ? (
+                  tickets.map((tickets) => (
+                    <p key={tickets.id}>{tickets.tipo}</p>
+                  ))
+                ) : (
+                  <>
+                    <p>Não localizamos nenhum ingresso</p>
+                    <button>COMPRAR INGRESSO</button>
+                  </>
+                )}
+              </>
+            )}
+            {closed && (
+                <p>Não localizamos nenhum ingresso</p>
+            )}
+          </div>
+        </section>
+      </main>
+    </>
+  );
 }
